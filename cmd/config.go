1b1ce3f985e10a360664218e666d4fb965787336
yekaterinika <yekaterina.nikolaeva2004@yandex.ru>
2024-01-07 16:06:33 +0300

    send messsage

--- cmd/config.go
+++ cmd/config.go
@@ -4,12 +4,12 @@ import (
 	"os"
 )
 
-type config struct {
-	mattermostToken string
-}
+// type config struct {
+// 	mattermostToken string
+// }
 
 func loadConfig() config {
 	var settings config
-	settings.mattermostToken = os.Getenv("MATTERMOST_TOKEN")
+	settings.MattermostToken = os.Getenv("MATTERMOST_TOKEN")
 	return settings
 }
--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type config struct {
+	MattermostToken string `json:"mattermost_token"`
+}
+
+type Message struct {
+	Text      string `json:"message"`
+	ChannelId string `json:"channel_id"`
+}
 
 func main() {
 	config := loadConfig()
-	fmt.Println(config.mattermostToken)
+	data, _ := json.Marshal(config)
+	var msg = Message{
+		Text:      "abacaba",
+		ChannelId: "9gs6do7otff9fmgcrktnk9opra",
+	}
+	bytesRepresentation, err := json.Marshal(msg)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	req, err := http.NewRequest("POST", "http://localhost:8065/api/v4/posts", bytes.NewBuffer(bytesRepresentation))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	var bearer = "Bearer " + config.MattermostToken
+	req.Header.Add("Authorization", bearer)
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	bytesResp, err := io.ReadAll(resp.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Println(string(bytesResp))
+
+	fmt.Printf("%s\n", data)
 }
