7c0e2bf430333f257ab13acb98e023fcff70041c
yekaterinika <yekaterina.nikolaeva2004@yandex.ru>
2024-03-19 01:53:10 +0300

    add suppressed

--- internal/client/http/jirahttpclient/jirahttpclient.go
+++ internal/client/http/jirahttpclient/jirahttpclient.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/base64"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -30,12 +31,7 @@ func NewJiraHttpClient(httpClient *http.Client, url string, username string, pas
 	}
 }
 
-func (client *JiraHttpClient) getIssueLink(bytes []byte, task jiratasks.JiraTaskCreationRequest) (string, error) {
-	var response jiratasks.JiraTaskCreationResponse
-	err := json.Unmarshal(bytes, &response)
-	if err != nil {
-		return "", err
-	}
+func (client *JiraHttpClient) getIssueLink(response jiratasks.JiraTaskCreationResponse, task jiratasks.JiraTaskCreationRequest) (string, error) {
 	link := client.baseUrl + "/projects/" + task.Fields.Project.Key + "/issues/" + response.Key
 	return link, nil
 }
@@ -43,22 +39,27 @@ func (client *JiraHttpClient) getIssueLink(bytes []byte, task jiratasks.JiraTask
 func (client *JiraHttpClient) CreateTask(task jiratasks.JiraTaskCreationRequest) (string, error) {
 	bytesRepresentation, err := json.Marshal(task)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf(err.Error() + " when attemp create jira issue marshal task")
 	}
 	req, err := http.NewRequest("POST", client.baseUrl+"/rest/api/2/issue/", bytes.NewBuffer(bytesRepresentation))
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf(err.Error() + " when attemp new request for create mattermost post")
 	}
 	req.Header.Add("Authorization", "Basic "+client.authorizationCode)
 	req.Header.Set("Content-Type", "application/json")
 	resp, err := client.httpClient.Do(req)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf(err.Error() + " when attemp do http request for create mattermost post")
 	}
 	bytesResp, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf(err.Error() + " when attemp create mattermost post")
 	}
 	log.Printf("Jira create task: %s", bytesResp)
-	return client.getIssueLink(bytesResp, task)
+	var response jiratasks.JiraTaskCreationResponse
+	err = json.Unmarshal(bytesResp, &response)
+	if err != nil {
+		return "", fmt.Errorf(err.Error() + " when attemp create mattermost post")
+	}
+	return client.getIssueLink(response, task)
 }
--- internal/client/http/mattermosthttpclient/mattermosthttpclient.go
+++ internal/client/http/mattermosthttpclient/mattermosthttpclient.go
@@ -3,6 +3,7 @@ package mattermosthttpclient
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -27,21 +28,21 @@ func NewHttpClient(client *http.Client, token string, baseUrl string) *Mattermos
 func (client *MattermostHttpClient) CreatePost(post mattermostmessages.RequestPost) error {
 	bytesRepresentation, err := json.Marshal(post)
 	if err != nil {
-		return err
+		return fmt.Errorf(err.Error() + " when attemp marshal message for creation mattermost post")
 	}
 	req, err := http.NewRequest("POST", client.mattermostBaseUrl+"/api/v4/posts", bytes.NewBuffer(bytesRepresentation))
 	if err != nil {
-		return err
+		return fmt.Errorf(err.Error() + " when attemp make new request message for creation mattermost post")
 	}
 	var bearer = "Bearer " + client.mattermostToken
 	req.Header.Add("Authorization", bearer)
 	resp, err := client.httpClient.Do(req)
 	if err != nil {
-		return err
+		return fmt.Errorf(err.Error() + " when attemp do request for creation mattermost post")
 	}
 	bytesResp, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return err
+		return fmt.Errorf(err.Error() + " when attemp read bytes for creation mattermost post")
 	}
 	log.Printf("MattermostBot create post: %s", bytesResp)
 	return nil
