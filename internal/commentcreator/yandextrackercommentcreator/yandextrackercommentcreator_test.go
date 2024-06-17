55b632319961a37d871d3689808b8d0b810ec13d
yekaterinika <yekaterina.nikolaeva2004@yandex.ru>
2024-06-17 23:43:38 +0300

    yandex comment creator fix tests:

--- internal/commentcreator/yandextrackercommentcreator/yandextrackercommentcreator_test.go
+++ internal/commentcreator/yandextrackercommentcreator/yandextrackercommentcreator_test.go
@@ -12,22 +12,45 @@ import (
 )
 
 func TestCreateComment(t *testing.T) {
+	tests := map[string]struct {
+		typeOrg           string
+		idOrg             string
+		typeAuthorization string
+		token             string
+		text              string
+		taskId            string
+	}{
+		"simple": {
+			typeOrg:           "TYPE-ORG",
+			idOrg:             "id-org",
+			typeAuthorization: "Bearer",
+			token:             "token",
+			text:              "text",
+			taskId:            "TEST-1",
+		},
+	}
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+				assert.Equal(t, req.URL.String(), "/v2/issues/"+tc.taskId+"/comments")
+				bufSize := 1024
+				buffer := make([]byte, bufSize)
+				length, _ := req.Body.Read(buffer)
+				assert.Equal(t, req.Header.Get(tc.typeOrg), tc.idOrg)
+				assert.Equal(t, req.Header.Get("Authorization"), tc.typeAuthorization+" "+tc.token)
+				var request yandextrackerhttpclient.RequestComment
+				json.Unmarshal(buffer[:length], &request)
+				assert.Equal(t, tc.text, request.Text)
+				rw.Write([]byte(`OK`))
+			}))
+			defer server.Close()
+			client := server.Client()
+			yandexTrackerHttpClient := yandextrackerhttpclient.NewYandexTracketHttpClient(server.URL,
+				server.URL, tc.idOrg, tc.typeOrg,
+				tc.typeAuthorization, tc.token, client)
+			commentCreator := NewYandexTrackerCommentCreator(yandexTrackerHttpClient)
+			commentCreator.CreateComment(context.Background(), tc.text, tc.taskId)
+		})
+	}
 
-	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		assert.Equal(t, req.URL.String(), "/v2/issues/TEST-1/comments")
-		bufSize := 1024
-		buffer := make([]byte, bufSize)
-		length, _ := req.Body.Read(buffer)
-		assert.Equal(t, req.Header.Get("TYPE-ORG"), "id-org")
-		assert.Equal(t, req.Header.Get("Authorization"), "Bearer token")
-		var request yandextrackerhttpclient.RequestComment
-		json.Unmarshal(buffer[:length], &request)
-		assert.Equal(t, "text", request.Text)
-		rw.Write([]byte(`OK`))
-	}))
-	defer server.Close()
-	client := server.Client()
-	yandexTrackerHttpClient := yandextrackerhttpclient.NewYandexTracketHttpClient(server.URL, server.URL, "id-org", "TYPE-ORG", "Bearer", "token", client)
-	commentCreator := NewYandexTrackerCommentCreator(yandexTrackerHttpClient)
-	commentCreator.CreateComment(context.Background(), "text", "TEST-1")
 }
