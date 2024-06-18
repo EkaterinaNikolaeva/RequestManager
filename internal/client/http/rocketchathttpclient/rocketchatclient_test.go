821721537c8b16721eabb1205b76c5ad5d614352
yekaterinika <yekaterina.nikolaeva2004@yandex.ru>
2024-06-18 13:28:45 +0300

    fix rocket chat http test:

--- internal/client/http/rocketchathttpclient/rocketchatclient_test.go
+++ internal/client/http/rocketchathttpclient/rocketchatclient_test.go
@@ -11,26 +11,46 @@ import (
 )
 
 func TestSendMessages(t *testing.T) {
-	msg := RequestMessage{
-		Message: RequestMessageData{Msg: "text",
-			Rid:  "chat-id",
-			Tmid: "root-msg-id",
+	tests := map[string]struct {
+		text   string
+		rid    string
+		tmid   string
+		userId string
+		token  string
+	}{
+		"simple": {
+			text:   "text",
+			rid:    "chat-id",
+			tmid:   "root-msg-id",
+			userId: "user-id",
+			token:  "token",
 		},
 	}
-	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		assert.Equal(t, req.URL.String(), "/api/v1/chat.sendMessage")
-		bufSize := 1024
-		buffer := make([]byte, bufSize)
-		length, _ := req.Body.Read(buffer)
-		assert.Equal(t, req.Header.Get("X-User-Id"), "user-id")
-		assert.Equal(t, req.Header.Get("X-Auth-Token"), "token")
-		var request RequestMessage
-		json.Unmarshal(buffer[:length], &request)
-		assert.Equal(t, msg, request)
-		rw.Write([]byte(`OK`))
-	}))
-	defer server.Close()
-	client := server.Client()
-	rocketChatClient := NewHttpClient(client, "user-id", "token", server.URL)
-	assert.Nil(t, rocketChatClient.SendMessage(context.Background(), msg))
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			msg := RequestMessage{
+				Message: RequestMessageData{Msg: tc.text,
+					Rid:  tc.rid,
+					Tmid: tc.tmid,
+				},
+			}
+			server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+				assert.Equal(t, req.URL.String(), "/api/v1/chat.sendMessage")
+				bufSize := 1024
+				buffer := make([]byte, bufSize)
+				length, _ := req.Body.Read(buffer)
+				assert.Equal(t, req.Header.Get("X-User-Id"), tc.userId)
+				assert.Equal(t, req.Header.Get("X-Auth-Token"), tc.token)
+				var request RequestMessage
+				json.Unmarshal(buffer[:length], &request)
+				assert.Equal(t, msg, request)
+				rw.Write([]byte(`OK`))
+			}))
+			defer server.Close()
+			client := server.Client()
+			rocketChatClient := NewHttpClient(client, tc.userId, tc.token, server.URL)
+			assert.Nil(t, rocketChatClient.SendMessage(context.Background(), msg))
+		})
+	}
+
 }
