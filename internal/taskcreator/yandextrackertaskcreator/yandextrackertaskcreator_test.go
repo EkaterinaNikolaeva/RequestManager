c4c59ca6d8a27b5ad3ba4f6398435b643983b613
yekaterinika <yekaterina.nikolaeva2004@yandex.ru>
2024-06-18 14:08:29 +0300

    yandex tracker creator test

--- internal/taskcreator/yandextrackertaskcreator/yandextrackertaskcreator_test.go
+++ internal/taskcreator/yandextrackertaskcreator/yandextrackertaskcreator_test.go
@@ -13,33 +13,59 @@ import (
 )
 
 func TestCreateTask(t *testing.T) {
-	task := task.TaskCreateRequest{
-		Description: "Description",
-		Type:        "Task",
-		Name:        "Name",
-		Project:     "Pr",
+	tests := map[string]struct {
+		idOrganization    string
+		typeOrganization  string
+		tokenType         string
+		token             string
+		taskCreateRequest task.TaskCreateRequest
+	}{
+		"simple": {
+			idOrganization:   "id-org",
+			typeOrganization: "TYPE-ORG",
+			tokenType:        "Bearer",
+			token:            "token",
+
+			taskCreateRequest: task.TaskCreateRequest{
+				Description: "Description",
+				Type:        "Task",
+				Name:        "Name",
+				Project:     "Pr",
+			},
+		},
 	}
-	taskApiYandexTracker := yandextrackerhttpclient.RequestTask{
-		Description: "Description",
-		Type:        "Task",
-		Summary:     "Name",
-		Queue:       "Pr",
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			taskApiYandexTracker := yandextrackerhttpclient.RequestTask{
+				Description: tc.taskCreateRequest.Description,
+				Type:        tc.taskCreateRequest.Type,
+				Summary:     tc.taskCreateRequest.Name,
+				Queue:       tc.taskCreateRequest.Project,
+			}
+			server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+				assert.Equal(t, req.URL.String(), "/v2/issues/")
+				bufSize := 1024
+				buffer := make([]byte, bufSize)
+				length, _ := req.Body.Read(buffer)
+				assert.Equal(t, req.Header.Get(tc.typeOrganization), tc.idOrganization)
+				assert.Equal(t, req.Header.Get("Authorization"), tc.tokenType+" "+tc.token)
+				var request yandextrackerhttpclient.RequestTask
+				json.Unmarshal(buffer[:length], &request)
+				assert.Equal(t, taskApiYandexTracker, request)
+				rw.Write([]byte(`OK`))
+			}))
+			defer server.Close()
+			client := server.Client()
+			yandexTrackerHttpClient := yandextrackerhttpclient.NewYandexTracketHttpClient(server.URL,
+				server.URL,
+				tc.idOrganization,
+				tc.typeOrganization,
+				tc.tokenType,
+				tc.token,
+				client)
+			taskCreator := NewYandexTrackerTaskCreator(yandexTrackerHttpClient)
+			taskCreator.CreateTask(context.Background(), tc.taskCreateRequest)
+		})
 	}
-	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		assert.Equal(t, req.URL.String(), "/v2/issues/")
-		bufSize := 1024
-		buffer := make([]byte, bufSize)
-		length, _ := req.Body.Read(buffer)
-		assert.Equal(t, req.Header.Get("TYPE-ORG"), "id-org")
-		assert.Equal(t, req.Header.Get("Authorization"), "Bearer token")
-		var request yandextrackerhttpclient.RequestTask
-		json.Unmarshal(buffer[:length], &request)
-		assert.Equal(t, taskApiYandexTracker, request)
-		rw.Write([]byte(`OK`))
-	}))
-	defer server.Close()
-	client := server.Client()
-	yandexTrackerHttpClient := yandextrackerhttpclient.NewYandexTracketHttpClient(server.URL, server.URL, "id-org", "TYPE-ORG", "Bearer", "token", client)
-	taskCreator := NewYandexTrackerTaskCreator(yandexTrackerHttpClient)
-	taskCreator.CreateTask(context.Background(), task)
+
 }
